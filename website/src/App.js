6b63dcf4b62a6db3c56f67ba2abf13d434bc9bb6
Tyler Anthony <tyle9682@TANTHONY.esri.com>
2018-07-19 08:55:57 -0400

    initial commit for home page

--- website/src/App.js
+++ website/src/App.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './Header/Header.js';
+import Home from './Home/Home.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Header/>
+          <Header />
         </header>
+        <Home />
       </div>
     );
   }
---
+++ website/src/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>Welcome home!</div>
+    );
+  }
+}
+
+export default Home;
