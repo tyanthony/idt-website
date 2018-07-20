dd5f92d2f9dcdfc6d77c01c81371c6cf2bc1e8c2
Tyler Anthony <tyle9682@TANTHONY.esri.com>
2018-07-20 08:03:14 -0400

    added more content to home page

--- website/src/Home/Home.js
+++ website/src/Home/Home.js
@@ -3,12 +3,22 @@ import React, { Component } from 'react';
 import './Home.css';
 
 class Home extends Component {
+
   render() {
-    const whoWeAre = <div className="whoWeAre">Who we are here</div>;
+    const intro = <div className="intro">Welcome to the Esri Vienna, VA R&amp;D Center!</div>;
+
+    const whoWeAre = 
+      (<div className="whoWeAre">
+        <p>
+          We are an Agile Software Development team working closely with the Defense and Intelligence community. We strive to bring 
+          intelligent and useful ArcGIS software to a wide variety of teams and organizations. Ultimately, our goal is to provide
+          helpful software that allows its users to successfully analyize and make decisions during critical events.
+        </p>
+      </div>);
 
     return (
       <div className="home">
-        Welcome home!
+        {intro}
         {whoWeAre}
       </div>
     );
