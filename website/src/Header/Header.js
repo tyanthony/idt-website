29877361b8be79f6a14d05305cffddfbf7f68cf2
Tyler Anthony <tanthony@esri.com>
2018-08-13 13:50:38 -0400

    routing working on basic level. added Projects and About pages

--- website/src/Header/Header.js
+++ website/src/Header/Header.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.css';
 
 class Header extends Component {
-  handleClick = () => {
-    console.log("clicked!");
+  handleClick = (item) => {
+    console.log(item + " was clicked");
   }
 
   render() {
-    const headerLinks = ["Home", "Projects", "About"];
-    const listItems = headerLinks.map((item) => 
-      <a onClick={this.handleClick} key={item} className="listItems">{item}</a>
-    );
+    // const headerLinks = ["Home", "Projects", "About"];
+    // const listItems = headerLinks.map((item) => 
+    //   <Link to={`/${item}`} key={item}><a onClick={this.handleClick(item)} className="listItems">{item}</a></Link>
+    // );
 
-    const links = <div className="headerLinks">{listItems}</div>
+    const homeLink = <Link to="/Home" className="listItems">Home</Link>;
+    const projectsLink = <Link to="/Projects" className="listItems">Projects</Link>;
+    const aboutLink = <Link to="/About" className="listItems">About</Link>;
+
+    const links = 
+      <div className="headerLinks">
+        {homeLink}
+        {projectsLink}
+        {aboutLink}
+      </div>
 
     const title = <div className="title">Esri Vienna R &amp; D</div>
 
--- website/src/index.js
+++ website/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import About from './About/About';
+import Projects from './Projects/Projects';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route path="/home" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
