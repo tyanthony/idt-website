43f4dca059e05636e975b74e23152d101c683058
Tyler Anthony <tanthony@esri.com>
2018-08-13 11:40:14 -0400

    styling on links in footer

--- website/src/Footer/Footer.js
+++ website/src/Footer/Footer.js
@@ -7,12 +7,12 @@ class Footer extends Component {
     const logo = <div className="logo">logo here</div>;
 
     const links = 
-      <div className="footerLinks">
-        <a >About</a>
-        <a >Contact Us</a>
-        <a href="https://www.esri.com/en-us/about/careers/job-search#@careerPath=@location=US-VA-Vienna@jobSearch=">Careers</a>
-        <a href="https://www.esri.com/en-us/home">Esri</a>
-        <a href="https://developers.arcgis.com/javascript/">ArcGIS API</a>
+      <div className="linksContainer">
+        <a className="footerLinks">About</a>
+        <a className="footerLinks">Contact Us</a>
+        <a className="footerLinks" href="https://www.esri.com/en-us/about/careers/job-search#@careerPath=@location=US-VA-Vienna@jobSearch=">Careers</a>
+        <a className="footerLinks" href="https://www.esri.com/en-us/home">Esri</a>
+        <a className="footerLinks" href="https://developers.arcgis.com/javascript/">ArcGIS API</a>
       </div>;
 
     return (
