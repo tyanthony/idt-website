d7c220e0b8fe13bd1b06fff75f14fb342ccb095c
Tyler Anthony <tanthony@esri.com>
2018-08-13 15:48:09 -0400

    added ReactFlipCard lib. beginning to implement card on About page

--- website/src/About/About.js
+++ website/src/About/About.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 import './About.css';
 
 class About extends Component {
+  constructor() {
+    super();
+    this.state = { isFlipped: false, };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+  }
+  
   render() {
-    return <div>ABOUT PAGE</div>;
+    return (
+
+      <ReactCardFlip isFlipped={this.state.isFlipped}>
+        <div key="front">
+          This is the FRONT
+          <button onClick={ this.handleClick }>Flip</button>
+        </div>
+
+        <div key="back">
+          This is the BACK
+          <button onClick={ this.handleClick }>Flip</button>
+        </div>
+      </ReactCardFlip>
+    );
   }
 }
 
