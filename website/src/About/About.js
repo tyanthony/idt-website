d072e46992259dddd33db034b4bc9eeaea75f44d
Tyler Anthony <tanthony@esri.com>
2018-08-14 09:02:18 -0400

    further work on About page. added link to About page in footer

--- website/src/About/About.js
+++ website/src/About/About.js
@@ -16,20 +16,38 @@ class About extends Component {
   }
   
   render() {
-    return (
-      <section className="flipcard-section">
-        <ReactCardFlip isFlipped={this.state.isFlipped}>
-          <div key="front" className="flipcard">
-            This is the FRONT
-            <button onClick={ this.handleClick }>Flip</button>
-          </div>
+    const mission = 
+      <div className="mission-wrapper">
+        <h1>Our Mission</h1>
+        <p>
+          Our mission is to be some pretty cool people who write software.
+        </p>
+      </div>;
+      
+    const cards = 
+      <div className="flipcard-wrapper">
+        <h1>Meet the Team</h1>
+        <section className="flipcard-section">
+          <ReactCardFlip isFlipped={this.state.isFlipped}>
+            <div key="front" className="flipcard">
+              This is the FRONT
+              <button onClick={ this.handleClick } className="flipcard-button">Flip</button>
+            </div>
 
-          <div key="back" className="flipcard">
-            This is the BACK
-            <button onClick={ this.handleClick }>Flip</button>
-          </div>
-        </ReactCardFlip>
-      </section>
+            <div key="back" className="flipcard">
+              This is the BACK
+              <button onClick={ this.handleClick } className="flipcard-button">Flip</button>
+            </div>
+          </ReactCardFlip>
+        </section>
+      </div>
+      ;
+
+    return (
+      <div className="about">
+        {mission}
+        {cards}
+      </div>
     );
   }
 }
--- website/src/Footer/Footer.js
+++ website/src/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Footer.css';
 
@@ -11,7 +12,7 @@ class Footer extends Component {
         <a className="footerLinks" href="https://www.esri.com/en-us/home">Esri</a>
         <a className="footerLinks" href="https://www.esri.com/en-us/about/careers/job-search#@careerPath=@location=US-VA-Vienna@jobSearch=">Careers</a>
         <a className="footerLinks" href="https://developers.arcgis.com/javascript/">ArcGIS API</a>
-        <a className="footerLinks">About</a>
+        <Link to="/about"><a className="footerLinks">About</a></Link>
         <a className="footerLinks">Contact Us</a>
       </div>;
 
