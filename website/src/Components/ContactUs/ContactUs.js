45c127ac36a2f52e1da0683bcb8f2a67ed4feaf5
Tyler Anthony <tanthony@esri.com>
2018-08-14 15:18:46 -0400

    inital work to have a contact page

---
+++ website/src/Components/ContactUs/ContactUs.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './ContactUs.css';
+
+class ContactUs extends Component {
+  render() {
+    return (
+      <div>
+        Contact Info here
+      </div>
+    );  
+  }
+}
+
+export default ContactUs;
--- website/src/Components/Footer/Footer.js
+++ website/src/Components/Footer/Footer.js
@@ -13,7 +13,7 @@ class Footer extends Component {
         <a className="footerLinks" href="https://www.esri.com/en-us/about/careers/job-search#@careerPath=@location=US-VA-Vienna@jobSearch=">Careers</a>
         <a className="footerLinks" href="https://developers.arcgis.com/javascript/">ArcGIS API</a>
         <Link to="/about" className="footerLinks">About</Link>
-        <a className="footerLinks">Contact Us</a>
+        <Link to="/contact-us" className="footerLinks">Contact Us</Link>
       </div>;
 
     return (
