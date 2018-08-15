6bde5bcb2a731aaab3362d97d323815d45600f30
Tyler Anthony <tanthony@esri.com>
2018-08-15 10:52:20 -0400

    added esri logo to footer and some additional styling

--- website/src/Components/Footer/Footer.js
+++ website/src/Components/Footer/Footer.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
 
 import './Footer.css';
 
 class Footer extends Component {
   render() {
-    const logo = <div className="logo">logo here</div>;
-
+    const logo = 
+      <div className="logo">
+        <Image src="http://ecoexplorer.arcgis.com/eco/images/esri.png" responsive className="logo-image"/>
+      </div>;
+    
     const links = 
-      <div className="linksContainer">
-        <a className="footerLinks" href="https://www.esri.com/en-us/home">Esri</a>
-        <a className="footerLinks" href="https://www.esri.com/en-us/about/careers/job-search#@careerPath=@location=US-VA-Vienna@jobSearch=">Careers</a>
-        <a className="footerLinks" href="https://developers.arcgis.com/javascript/">ArcGIS API</a>
-        <Link to="/about" className="footerLinks">About</Link>
-        <Link to="/contact-us" className="footerLinks">Contact Us</Link>
+      <div className="links-container">
+        <h4 className="links-title">Learn more</h4>
+        <a className="footer-links" href="https://www.esri.com/en-us/home">Esri</a>
+        <a className="footer-links" href="https://www.esri.com/en-us/about/careers/job-search#@careerPath=@location=US-VA-Vienna@jobSearch=">Careers</a>
+        <a className="footer-links" href="https://developers.arcgis.com/javascript/">ArcGIS API</a>
+        <Link to="/about" className="footer-links">About</Link>
+        <Link to="/contact-us" className="footer-links">Contact Us</Link>
       </div>;
 
     return (
-      <div className="footerContainer">
+      <div className="footer-container">
         {logo}
         {links}
       </div>
